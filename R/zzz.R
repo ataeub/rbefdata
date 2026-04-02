d3b02380d22283144206ad52b712e113082da48b
ataeub <alexander.taeuber@idiv.de>
2026-04-02 17:18:37 +0200

    Change to current working URL

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad = function(lib, pkg) {
   .bef.env$.bef.opts = list(
-    url = "http://china.befdata.biow.uni-leipzig.de",
+    url = "http://data.botanik.uni-halle.de/bef-china",
     tematres_url = "http://tematres.befdata.biow.uni-leipzig.de/vocab/index.php",
     tematres_service_url = "http://tematres.befdata.biow.uni-leipzig.de/vocab/services.php",
     download_dir = "downloads",
